7354bbe0785ffb78ba715b6da1bce0b0f1186703
kasthurikaliappan <kasthuridivya97@gmail.com>
2017-07-07 15:14:16 +0530

    Add files via upload

---
+++ maximum.java
@@ -0,0 +1,25 @@
+package gk;
+import java.util.*;
+public class maximum {
+public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<a.length;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		Arrays.sort(a);
+		System.out.print("[");
+		for(int i=1;i<=3;i++)
+		{
+			System.out.print(a[n-i]+",");
+		}
+		System.out.print("]");
+		
+
+	}
+
+}
+
