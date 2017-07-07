a7286ea798f7f411444c25b8ee4326481a464c12
kasthurikaliappan <kasthuridivya97@gmail.com>
2017-07-07 14:42:29 +0530

    Add files via upload

---
+++ hun1.java
@@ -0,0 +1,25 @@
+package gk;
+import java.util.*;
+public class hun1 {
+	 public static void main(String args[])
+	    {
+	        Scanner in=new Scanner(System.in);
+	        String str=in.nextLine();
+			int c1=0;
+			String b[]=str.split(" ");
+			int c=b.length;
+			for(int i=0;i<c;i++)
+	                {
+				char a[]=b[i].toCharArray();
+				c1=a.length;
+				for(int j=c1-1;j>=0;j--)
+	                        {
+					System.out.print(a[j]);
+				}
+				System.out.print(" ");
+			}
+
+	    }
+	    
+	}
+
