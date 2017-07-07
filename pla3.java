9e4db47bdfacce064fe81221d99cb02346e38e59
kasthurikaliappan <kasthuridivya97@gmail.com>
2017-07-07 12:43:18 +0530

    Add files via upload

---
+++ pla3.java
@@ -0,0 +1,27 @@
+package gk;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class pla3 {
+	 public static void main(String arg[])
+	    {
+	        Scanner sc=new Scanner(System.in);
+	        int n =sc.nextInt();
+	        int s=0,r=0;
+	        while(n>1)
+	        {
+	            r=n%10;
+	            s=s/10;
+	            n=(r*r)+(s*s);
+	        }
+	            if(n==1)
+	            {
+	                System.out.println("HAPPY NUMBER");
+	            }
+	            else{
+	                System.out.println("NO HAPPY NUMBER");
+	            }
+	        
+	    }
+	}
+		
