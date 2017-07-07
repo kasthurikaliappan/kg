10bc04f76c0fcae59a2a16b74095b91657c7ce5f
kasthurikaliappan <kasthuridivya97@gmail.com>
2017-07-07 10:37:19 +0530

    Add files via upload

---
+++ pro2.java
@@ -0,0 +1,28 @@
+package gk;
+import java.util.*;
+public class pro2 {
+ public static void main(String args[]) {
+	 
+			Scanner in=new Scanner(System.in);
+			int n=in.nextInt();
+			int temp=n;
+			int k=in.nextInt();
+			in.close();
+			int count=0;
+			while(temp!=0){
+				count++;
+				temp/=10;
+			}
+			int[] array=new int[count];
+			for(int i=0;i<count;i++){
+				array[i]=n%10;
+				n/=10;
+			}
+			Arrays.sort(array);
+			for(int i=0;i<(count-k);i++){
+				System.out.print(array[i]);
+			}
+		}
+
+	}
+	 
