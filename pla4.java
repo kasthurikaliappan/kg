79f465899d7c4033dc3ef1fc8a72b3e5b28e8bc5
kasthurikaliappan <kasthuridivya97@gmail.com>
2017-07-07 12:48:10 +0530

    Add files via upload

---
+++ pla4.java
@@ -0,0 +1,18 @@
+package gk;
+import java.util.*;
+public class pla4 {
+	public static void main(String args[]){
+		Scanner in =new Scanner(System.in);
+		System.out.println("enter the string");
+		String s=in.nextLine();
+		String res;
+		StringBuffer sb=new StringBuffer(s);
+		sb.reverse();
+		String s1=sb.toString();
+		System.out.println(s1);
+		 res = s1.replaceAll("[aeiouAEIOU]", "");
+		  System.out.println(res);  
+	 
+	}
+	}
+
