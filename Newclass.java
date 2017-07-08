a6adaa80477cf756a41364b13ed41e8986bd1535
kasthurikaliappan <kasthuridivya97@gmail.com>
2017-07-08 11:17:25 +0530

    Add files via upload

---
+++ Newclass.java
@@ -0,0 +1,11 @@
+package gk;
+
+public class Newclass {
+	public static void main(String args[])
+	{
+	String str="this is java programming" ;
+	int temp=str.length();
+	System.out.println(temp);
+	}
+	}
+
